b684a62c90ffb3efcc9b6a4715eb01094b647499
Soyeon <hothemp35@gmail.com>
2022-05-02 00:12:32 +0900

    second commit(리베이스 테스트)
    third commit(rebase)

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 function App() {
-  return <div></div>;
+  return <div>asd</div>;
 }
 
 export default App;
--- src/components/ColorBox.js
+++ src/components/ColorBox.js
@@ -1,15 +1,16 @@
-import ColorContext from "../contexts/color";
+import ColorContext from '../contexts/color';
 
 const ColorBox = () => {
-  return (
-    <ColorContext.Consumer>
-      {(value) => (
-        <div
-          style={{ width: "64px", height: "64px", background: value.color }}
-        />
-      )}
-    </ColorContext.Consumer>
-  );
+	return (
+		<ColorContext.Consumer>
+			{(value) => (
+				<div
+					style={{ width: '64px', height: '64px', background: value.color }}
+				/>
+			)}
+			<div>커밋1</div>
+		</ColorContext.Consumer>
+	);
 };
 
 export default ColorBox;
